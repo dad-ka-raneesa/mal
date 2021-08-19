df9b307d2d3ec0858a0e56e0618d916f088a5c10
KarriBhuvanaDeepika <bhuvanadeepika017@gmail.com>
2021-08-19 12:38:57 +0530

    Add List type and printing it

---
+++ impls/bd/types.js
@@ -0,0 +1,15 @@
+class MalValue {
+}
+
+class List extends MalValue {
+  constructor(ast) {
+    super();
+    this.ast = ast;
+  }
+
+  toString() {
+    return '(' + this.ast.map(x => x.toString()).join(' ') + ')';
+  }
+}
+
+module.exports = { List };
