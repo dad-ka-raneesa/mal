9c560d4c40eb7ce39ef486bd80fc37548cec4f2f
KarriBhuvanaDeepika <bhuvanadeepika017@gmail.com>
2021-08-19 11:02:37 +0530

    Add ability to read integer and floating point numbers

---
+++ impls/bd/reader.js
@@ -0,0 +1,40 @@
+class Reader {
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.position = 0;
+  }
+
+  peek() {
+    return this.tokens[this.position];
+  }
+
+  next() {
+    const token = this.peek();
+    if (this.position < this.tokens.length) this.position++;
+    return token;
+  }
+}
+
+const tokenize = (str) => {
+  const regEx = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]*)/g;
+  return [...str.matchAll(regEx)].map(x => x[1]).slice(0, -1);
+}
+
+const read_form = (reader) => {
+  const token = reader.peek();
+  if (token.match(/^-?[0-9]+$/)) {
+    return parseInt(token);
+  }
+  if (token.match(/^-?[0-9][0-9.]*$/)) {
+    return parseFloat(token);
+  }
+  return token;
+}
+
+const read_str = (str) => {
+  const tokens = tokenize(str);
+  const reader = new Reader(tokens);
+  return read_form(reader);
+}
+
+module.exports = { read_str };
---
+++ impls/bd/step1_read_print.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const { read_str } = require('./reader');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const READ = (str) => read_str(str);
+
+const EVAL = (str) => str;
+
+const PRINT = (str) => str;
+
+const rep = (str) => PRINT(EVAL(READ(str)));
+
+const loop = () => {
+  rl.question("user> ", (str) => {
+    console.log(rep(str));
+    loop();
+  })
+}
+
+loop();
